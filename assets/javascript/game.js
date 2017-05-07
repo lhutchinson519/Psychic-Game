42487be513aa8f9aedcdaf85c91e92308faa408b
Lauren Hutchinson <Lauren Hutchinson>
2017-05-07 08:22:19 -0500

    final psychic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,66 +7,67 @@ var losses=0;
 var guesses=9;
 var guessesLeft=9;
 var guessedLetters =[];
-var letterToGuess = null;
 
-//hTML identifiers
-var wins = document.getElementById("wins")
-var losses = document.getElementById("losses")
-var guessesLeft = document.getElementById("guessesLeft")
-var soFar = document.getElementById("soFar")
-
-//function for when someone pushes a key
-document.onkeyup = function(event) {
-    guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 // Sets the computerGuess variable equal to a random choice from the computerChoice array.
 var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+console.log("computer guess:" + computerGuess);
 
-//equaling the HTML 
-var updateGuessLeft = function(){
-	document.querySelector('guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
-};
+//function for when someone pushes a key
+document.onkeyup = function(event){
 
-var updateletterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoice.length)];
-};
+	 var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	 console.log("guess:" + userGuess);
 
-var updateguessedLetters = function() { 
-  document.querySelector('soFar').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-};
+	 //check if guessed before
+	 if(guessedLetters.indexOf(userGuess) == -1){
+	 	console.log("not guessed");
 
+	    guessesLeft--;
+	    document.getElementById("guessesLeft").innerHTML = guessesLeft.toString();
+	    console.log(guessesLeft);
+
+	//add userGuess to guessed letter
+		guessedLetters.push(userGuess);
+
+	//updated htmlguessed with guessed letters
+		document.getElementById("guessedLetters").innerHTML = guessedLetters.toString();
+
+
+			if (guessesLeft > 0){
+				console.log("guesses left");
+			            if (userGuess == computerGuess){
+			                wins++;
+			                document.querySelector('#wins').innerHTML = wins;
+			                alert("Yes, you are psychic!");
+			                reset();
+			            }
+			            else{guesses--;}
+			}
+
+			else if(guessesLeft == 0){
+				console.log("no guesses left");
+		 // or we lose and update the html to display the loss 
+			    losses++;
+			       document.querySelector('#losses').innerHTML = losses;
+			            alert("Sorry, you lose, maybe try again");
+			 //reset. 
+			       reset();}
+	}
+	 else{
+	 	console.log("guessed");
+	 }
+
+}
 //to reset
-var reset = function() {
-  totalGuesses = 9;
+function reset() {
+  //totalGuesses = 9;
   guessesLeft = 9;
   guessedLetters = [];
 
-
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
+document.getElementById("guessesLeft").innerHTML = guessesLeft.toString();
+document.getElementById("guessedLetters").innerHTML = guessedLetters.toString();
 }
-
-
-//making sure the choice is within the array in order to change the numbers on the page
-  if (guessesLeft > 0){
-            if (userGuess == letterToGuess){
-                wins++;
-                document.querySelector('wins').innerHTML = "Wins: " + wins;
-                alert("Yes, you are psychic!");
-                reset();
-            }
-        }else if(guessesLeft == 0){
-            // or we lose and update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, you lose, maybe try again");
-            //reset. 
-            reset();
-        };
-}
-
 
 
 
